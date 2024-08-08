39cb7e0ba31ee84d98399e85a75d13c66bb9036a
saikrishnask15 <saikrishnabakkagoni@gmail.com>
2024-08-08 12:28:22 +0530

    1st commit

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,18 +1,21 @@
-import { faHome, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import { faHome, faShoppingCart, faX} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SideBar = ({close}) => {
   return (
     <div className="side-bar-ctn" onClick={close}>
-        {/* <div className="sidebar-btn">
-        <FontAwesomeIcon icon={faX}/>
-        </div> */}
+        <div className="sidebar-btn">
+        <h2>Shoppie</h2>
+        {/* <FontAwesomeIcon icon={faX}/> */}
+        </div>
+        <div className="sidebar-links">
       <a href="/">
         <FontAwesomeIcon icon={faHome} /> Home
       </a>
       <a href="/cart">
         <FontAwesomeIcon icon={faShoppingCart} /> Cart
       </a>
+      </div>
     </div>
   );
 };
